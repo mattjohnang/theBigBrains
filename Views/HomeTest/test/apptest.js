f7f501d2e4869009410255010935bf5bca1845c9
A01203501 <60677637+A01203501@users.noreply.github.com>
2020-05-05 18:44:38 -0700

    Add files via upload
    updated the test file to increase code coverage

--- Views/HomeTest/test/apptest.js
+++ Views/HomeTest/test/apptest.js
@@ -4,6 +4,9 @@ const colorShift = require('../colours.js').colorShift
 const endGame = require('../colours.js').endGame
 const testMocha = require('../colours.js').testMocha
 const checkAnswers = require('../colours.js').checkAnswers
+const createCheckButton = require('../colours.js').createCheckButton
+const createButtons = require('../colours.js').createButtons
+const initializeDifficultySelect = require('../colours.js').initializeDifficultySelect
 
 
 
@@ -18,14 +21,23 @@ describe('App', function(){
     it('app should return a string containing an rgb color value', function() {
         assert.isString(colorShift('button'))
     })
-    it('app should return a boolean value'), function(){
+    it('app should return a boolean value', function(){
         assert.isBoolean(endGame(true))
-    }
+    })
     it('app should return a true boolean', function() {
         assert.equal(checkAnswers(['rgb(204, 0, 0)', 'rgb(0, 204, 0)', 'rgb(0, 0, 204)', 'rgb(255, 255, 0)', 'rgb(128, 128, 0)', 'rgb(255, 128, 0)', 'rgb(0, 0, 0)', 'rgb(255, 255, 255)'], ['rgb(204, 0, 0)', 'rgb(0, 204, 0)', 'rgb(0, 0, 204)', 'rgb(255, 255, 0)', 'rgb(128, 128, 0)', 'rgb(255, 128, 0)', 'rgb(0, 0, 0)', 'rgb(255, 255, 255)'], 8), true)
     })
     it('app should return a false boolean', function() {
         assert.equal(checkAnswers([''], ['rgb(204, 0, 0)', 'rgb(0, 204, 0)', 'rgb(0, 0, 204)', 'rgb(255, 255, 0)', 'rgb(128, 128, 0)', 'rgb(255, 128, 0)', 'rgb(0, 0, 0)', 'rgb(0, 0, 0)'], 8), false)
+    })
+    it('app should return a string value containing <button id="check">Check Answer</button>', function() {
+        assert.equal(createCheckButton(), '<button id="check">Check Answer</button>')
+    })
+    it('app should return a string containing #button1', function() {
+        assert.equal(createButtons(1), '#button1')
+    })
+    it('app should return a string containing <button id="buttonCount">Confirm</button>', function() {
+        assert.equal(initializeDifficultySelect(), '<button id="buttonCount">Confirm</button>')
     })
 
 })
