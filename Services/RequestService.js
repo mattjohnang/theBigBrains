7d78439328be1318b5b1d33c9d427b36ff815974
mattjohnang <55511044+mattjohnang@users.noreply.github.com>
2020-05-06 15:53:46 -0700

    Update RequestService.js

--- Services/RequestService.js
+++ Services/RequestService.js
@@ -9,8 +9,7 @@ class RequestService {
     reqHelper(req) {
         // Send username and login status to view if authenticated.
         if(req.isAuthenticated()) {
-            return { authenticated: true, username : req.user.username, firstName: req.user.firstName, 
-                lastName: req.user.lastName, email: req.user.email, id: req.user._id };
+            return { authenticated: true, username : req.user.username, email: req.user.email, id: req.user._id };
         }
         // Send logged out status to form if not authenticated.
         else {  
