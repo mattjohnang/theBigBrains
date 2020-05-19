a5a11808c79705d6a6ece3ca0a61de209efb9b8e
mattjohnang <55511044+mattjohnang@users.noreply.github.com>
2020-05-19 16:34:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'static')));
 
 
+app.use(express.static(__dirname+'/public'));
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 
 
